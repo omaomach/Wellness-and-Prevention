3be7b94ab3214d27cb93e65754c8a187674a1073
Jamie Codez <cruiseomondi90@gmail.com>
2022-07-06 19:56:45 +0300

    Branching out of product

--- src/main/java/com/omao/wellness/product/model/Product.java
+++ src/main/java/com/omao/wellness/product/model/Product.java
@@ -18,16 +18,16 @@ import static javax.persistence.GenerationType.IDENTITY;
 public class Product {
     @Id
     @GeneratedValue(strategy = IDENTITY)
-    @Column(name = "id",nullable = false)
+    @Column(name = "id", nullable = false)
     private Long id;
-    @Column(name = "product_name",nullable = false)
+    @Column(name = "product_name", nullable = false)
     private String productName;
-    @Column(name = "product_description",nullable = false)
+    @Column(name = "product_description", nullable = false)
     private String productDescription;
-    @Column(name = "suggested_use",nullable = false)
+    @Column(name = "suggested_use", nullable = false)
     private String suggestedUse;
     @ElementCollection
     @CollectionTable(name = "ingredients", joinColumns = @JoinColumn(name = "id"))
-    @Column(name = "ingredients",nullable = false)
+    @Column(name = "ingredients", nullable = false)
     private Collection<Ingredient> ingredients;
 }
